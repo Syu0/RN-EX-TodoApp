091dd1cd4dc26faa71706cd1d756e87a9ccdb4da
cozy <meaningful.jy@gmail.com>
2022-07-07 01:18:11 +0900

    splited components with header Todo added TabBar(active , complate)

--- TodoApp1/App.js
+++ TodoApp1/App.js
@@ -12,45 +12,44 @@ import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  Text,
   View,
   TextInput,
-  useColorScheme,
-  ScrollView,
   Alert,
-  TouchableOpacity,
 } from 'react-native';
 import {theme} from './colors.js';
-
+import Header from './app/Header.js';
+import Todo from './app/Todo.js';
+import TabBar from './app/TabBar.js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {} from 'react';
 import {loadOptions} from '@babel/core';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 const STORIGE_KEY_TODOS = '@toDos';
 const WORK_MODE = '@working';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
   const [working, setWorking] = useState(false);
   const [text, setText] = useState('');
-  const [done, setDone] = useState('');
+  const [type, setType] = useState('Active');
   const work = () => setWorking(true);
   const travel = () => setWorking(false);
   const [Todos, setTodos] = useState({});
+
   const onChangeText = payload => {
     setText(payload);
   };
+
   const saveTodos = async toSave => {
     await AsyncStorage.setItem(STORIGE_KEY_TODOS, JSON.stringify(toSave));
   };
+
   const loadToDos = async () => {
     try {
       const s = await AsyncStorage.getItem(STORIGE_KEY_TODOS);
       setTodos(JSON.parse(s));
       console.log(JSON.parse(s));
     } catch (error) {
-      console.log(error);
+      console.log('no data from STORIGE', error);
     }
   };
 
@@ -65,6 +64,15 @@ const App: () => Node = () => {
     await saveTodos(newTodos);
     setText('');
     console.log(newTodos);
+  };
+
+  const toggleTodo = async id => {
+    const newTodos = Object.assign({}, Todos);
+    console.log(id, 'bbbbbb');
+    newTodos[id].done = !newTodos[id].done;
+
+    setTodos(newTodos);
+    await saveTodos(newTodos);
   };
 
   const deleteTodo = async id => {
@@ -110,24 +118,6 @@ const App: () => Node = () => {
     await saveMode(mode);
   };
 
-  const doneTodo = async id => {
-    const newTodos = Object.assign({}, Todos);
-    newTodos[id].done = true;
-    console.log('done , ', newTodos[id]);
-
-    setTodos(newTodos);
-    await saveTodos(newTodos);
-  };
-
-  const notDone = async id => {
-    const newTodos = Object.assign({}, Todos);
-    newTodos[id].done = false;
-    console.log('not done , ', newTodos[id]);
-
-    setTodos(newTodos);
-    await saveTodos(newTodos);
-  };
-
   const editTodo = id => {
     console.log(Todos[id].text);
     return (
@@ -143,35 +133,11 @@ const App: () => Node = () => {
     initMode();
     loadToDos();
   }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="auto" />
-      <View style={styles.header}>
-        <TouchableOpacity
-          onPress={() => {
-            setMode('work');
-          }}>
-          <Text
-            style={{
-              ...styles.btnText,
-              color: working ? 'white' : theme.grey,
-            }}>
-            Work
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            setMode('travel');
-          }}>
-          <Text
-            style={{
-              ...styles.btnText,
-              color: !working ? 'white' : theme.grey,
-            }}>
-            Travel
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <Header mode={working} setMode={setMode} />
       <View>
         <TextInput
           style={styles.textinput}
@@ -181,91 +147,23 @@ const App: () => Node = () => {
           onSubmitEditing={addTodo}
         />
       </View>
-      <ScrollView>
-        {Object.keys(Todos).map(key =>
-          working === Todos[key].working ? (
-            Todos[key].done != true ? (
-              <View key={key} style={styles.todo}>
-                <TouchableOpacity
-                  style={styles.btns}
-                  onPress={() => {
-                    doneTodo(key);
-                  }}>
-                  <Text>⚪</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.textarea}
-                  onPress={() => {
-                    editTodo(key);
-                  }}>
-                  <Text style={styles.todoText}>{Todos[key].text}</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                  style={styles.btns}
-                  onPress={() => {
-                    deleteTodo(key);
-                  }}>
-                  <Icon name="trash" size={20} color="#4a8bfc" />
-                </TouchableOpacity>
-              </View>
-            ) : null
-          ) : null,
-        )}
-        {Object.keys(Todos).map(key =>
-          working === Todos[key].working ? (
-            Todos[key].done === true ? (
-              <View key={key} style={styles.doneTodo}>
-                <TouchableOpacity
-                  style={styles.btns}
-                  onPress={() => {
-                    notDone(key);
-                  }}>
-                  <Text>🔘</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.doneTodoTextArea}>
-                  <Text>{Todos[key].text}</Text>
-                </TouchableOpacity>
-
-                <View style={styles.btns}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      deleteTodo(key);
-                    }}>
-                    <Icon name="trash" size={20} color="#4a8bfc" />
-                  </TouchableOpacity>
-                </View>
-              </View>
-            ) : null
-          ) : null,
-        )}
-      </ScrollView>
+      <Todo
+        Todos={Todos}
+        toggleTodo={toggleTodo}
+        editTodo={editTodo}
+        deleteTodo={deleteTodo}
+        type={type}
+        working={working}
+      />
+      <TabBar setType={setType} type={type} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {flex: 1, paddingHorizontal: 20, backgroundColor: theme.darkbg},
-  header: {
-    backgroundColor: theme.darkbg,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-  },
-  btns: {
-    flexDirection: 'row',
-    alignContent: 'flex-end',
-    flex: 1,
-  },
-  btnText: {
-    fontSize: 38,
-    fontWeight: '600',
-  },
   textarea: {
     flex: 7,
-  },
-  highlight: {
-    fontWeight: '700',
   },
   textinput: {
     backgroundColor: theme.lightbg,
@@ -275,45 +173,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 10,
     fontSize: 17,
-  },
-  todo: {
-    backgroundColor: theme.todobg,
-    marginBottom: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 15,
-    flexDirection: 'row',
-    alignContent: 'flex-start',
-  },
-  doneTodo: {
-    backgroundColor: theme.grey,
-    marginBottom: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 15,
-    flexDirection: 'row',
-    alignContent: 'flex-start',
-  },
-  doneTodoTextArea: {
-    flex: 7,
-    flexDirection: 'row',
-    alignContent: 'flex-start',
-  },
-  doneTodoText: {
-    flex: 1,
-    color: 'black',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  todoText: {
-    flex: 1,
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  btn: {
-    marginRight: 5,
-    flex: 1,
   },
 });
 
--- TodoApp1/app/TabBar.js
+++ TodoApp1/app/TabBar.js
@@ -1,21 +1,31 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import TabBarItem from './TabBarItem';
 const TabBar = ({setType, type}) => (
-  <View style={StyleSheet.container}>
+  <View style={styles.container}>
     <TabBarItem type={type} title="All" setType={() => setType('All')} />
-    <TabBarItem type={type} title="All" border setType={() => setType('Active')} />
-    <TabBarItem type={type} title="All" border setType={() => setType('Complate')} />
+    <TabBarItem
+      type={type}
+      title="Active"
+      border
+      setType={() => setType('Active')}
+    />
+    <TabBarItem
+      type={type}
+      title="Complate"
+      border
+      setType={() => setType('Complate')}
+    />
   </View>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        height:70,
-        flexDirection:'row',
-        borderTopWidth:1,
-        borderTopColor:'#dddddd',
-    }
+  container: {
+    height: 70,
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: '#dddddd',
+  },
 });
 
-export default TabBar
+export default TabBar;
